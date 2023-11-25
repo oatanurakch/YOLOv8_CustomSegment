a0edfdf27565904cccc80819d4a5a6ff04dd99a9
oatanurakch <oatty6106164@gmail.com>
2023-11-25 21:56:14 +0700

    update centroid point

--- testYOLOv8.py
+++ testYOLOv8.py
@@ -1,33 +1,50 @@
 from ultralytics import YOLO as yolo
 import cv2
+import numpy as np
 
 # Initialize
 model = yolo('yolov8s-seg.pt')
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
 
 while cap.isOpened():
     ret, frame = cap.read()
     
+    # Check if frame is not empty
     if ret:
         # Inference
-        results = model(source = frame, conf = 0.7, save = False, classes = 0)
+        results = model(source = frame, conf = 0.7, save = False)
 
         # Check if there is any object detected
-        if results[0].pred is None:
+        if not len(results[0]):
             print('No object detected')
-            continue
+            annotated_frame = frame
 
-        # Plotting the result
-        annotated_frame = results[0].plot()
+        else:
+            # Plotting the result
+            annotated_frame = results[0].plot()
 
-        # Add Point to the frame
-        for r in results:
-            mask = results[0].masks.xy
-            for i in range(len(mask)):
-                for j in range(len(mask[i])):
-                    # print(f'x: {mask[i][j][0]}, y: {mask[i][j][1]}')
-                    cv2.circle(annotated_frame, (int(mask[i][j][0]), int(mask[i][j][1])), 1, (0, 255, 0), 2)
+            # Add Point to the frame
+            for r in results:
+                mask = results[0].masks.xy
+                for i in range(len(mask)):
+                    # Point quantity
+                    p_qty = len(mask[i])
+                    # Store point coordinate
+                    x_arr = []
+                    y_arr = []
+                    for j in range(len(mask[i])):
+                        # print(f'x: {mask[i][j][0]}, y: {mask[i][j][1]}')
+                        # append x and y to array
+                        x_arr.append(mask[i][j][0])
+                        y_arr.append(mask[i][j][1])
+                    # Calculate centroid
+                    x_arr = np.array(x_arr)
+                    y_arr = np.array(y_arr)
+                    p_centroid = (int(np.sum(x_arr) / p_qty), int(np.sum(y_arr) / p_qty))
+                    # Draw point and text
+                    cv2.putText(annotated_frame, f'{p_centroid}', (p_centroid[0] - 5, p_centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+                    cv2.circle(annotated_frame, (p_centroid[0], p_centroid[1]), 1, (255, 255, 255), 2)
         
         # Display the frame
         cv2.imshow('YOLOv8', annotated_frame)
